7455ec83bcf29a86e0600b42d56cfb4c12ceb95b
evenjay42014-stack <evenjay42014@gmail.com>
2026-09-17 04:48:03 -0700

    Add review_council chunk rc_5 and loader

---
+++ arena/review_council.py
@@ -0,0 +1,9 @@
+"""Multi-model project/app review council (loaded from chunks)."""
+from arena._chunks.rc_0 import CHUNK as C0
+from arena._chunks.rc_1 import CHUNK as C1
+from arena._chunks.rc_2 import CHUNK as C2
+from arena._chunks.rc_3 import CHUNK as C3
+from arena._chunks.rc_4 import CHUNK as C4
+from arena._chunks.rc_5 import CHUNK as C5
+import base64 as _b64
+exec(compile(_b64.b64decode(C0+C1+C2+C3+C4+C5), __file__, "exec"), globals())
